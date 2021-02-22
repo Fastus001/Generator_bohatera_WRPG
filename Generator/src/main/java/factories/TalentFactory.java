dbad70165514f5e1ef726d1ee6eb8a0d6fa7fc02
Fastus <fastus6666@gamil.com>
2021-02-22 09:53:54 +0100

    Created TalentFactory. Closes #43.

---
+++ Generator/src/main/java/factories/TalentFactory.java
@@ -0,0 +1,40 @@
+package factories;
+
+import commons.Talent;
+import utilities.Reader;
+
+import java.util.List;
+
+import static java.lang.Integer.parseInt;
+
+public class TalentFactory {
+    private static TalentFactory instance;
+    private static List<String[]> rawTalents;
+
+    private TalentFactory() {
+        rawTalents = Reader.getListOfArraysFrom( "talenty.txt" );
+    }
+
+    public static TalentFactory getInstance(){
+        if(instance == null){
+            instance =  new TalentFactory();
+        }
+        return instance;
+    }
+
+
+    public Talent createTalent(String name){
+        return rawTalents.stream().filter( s->s[0].equals( name ) )
+                .findFirst()
+                .map( this::mapTalent )
+                .orElseThrow();
+    }
+
+    private Talent mapTalent(String[] o) {
+        return Talent.builder()
+                .name( o[0] )
+                .relatedStat( parseInt( o[1] ) )
+                .test( o[2] )
+                .description( o[3] ).build();
+    }
+}
---
+++ Generator/src/test/java/factories/TalentFactoryTest.java
@@ -0,0 +1,32 @@
+package factories;
+
+import commons.Talent;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TalentFactoryTest {
+    TalentFactory talentFactory;
+
+    @BeforeEach
+    void setUp() {
+        talentFactory = TalentFactory.getInstance();
+    }
+
+    @Test
+    void getInstance() {
+        TalentFactory newInstance = TalentFactory.getInstance();
+
+        assertEquals( talentFactory,newInstance );
+    }
+
+    @Test
+    void createTalent() {
+        Talent createdTalent = talentFactory.createTalent( "Wytwórca" );
+
+        assertAll( ()->assertEquals( "Wytwórca",createdTalent.getName() ),
+                   ()->assertEquals( 6,createdTalent.getRelatedStat()),
+                   ()->assertEquals( "Rzemiosło (Dowolne)",createdTalent.getTest()));
+    }
+}
