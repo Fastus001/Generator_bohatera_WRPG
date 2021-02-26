b2c30006fc4e30429935278d40e64fc8a88bf780
Fastus <fastus6666@gamil.com>
2021-02-26 10:40:36 +0100

    Create test for Hero Progress. Closes #48.

--- Generator/src/test/java/hero/HeroProgressTest.java
+++ Generator/src/test/java/hero/HeroProgressTest.java
@@ -10,19 +10,26 @@ import factories.ProfessionFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Matchers.anyInt;
+import static org.mockito.Mockito.*;
 
 class HeroProgressTest {
 
     public static final String GLADIATOR_GLADIATORKA = "GLADIATOR/GLADIATORKA";
+    public static final int MIN_LEVEL_ONE = 5;
     HeroProgress progress;
+
     Hero hero;
 
     @BeforeEach
     void setUp() {
+
         hero = HeroFactory.getInstance()
                 .create( RaceType.HUMAN, GLADIATOR_GLADIATORKA, Gender.FEMALE);
         progress = new HeroProgress( hero );
@@ -30,38 +37,48 @@ class HeroProgressTest {
 
     @Test
     void newProfession() {
+        progress.newProfession( ProfessionFactory
+                                        .getInstance()
+                                        .create(GLADIATOR_GLADIATORKA,2) );
+
+        long numberOfProfSkills = hero.getKnownSkills().stream()
+                .filter( Skill::isProfession )
+                .count();
+
+        assertAll( ()->assertEquals( 2,hero.getProfession().getLevel() ),
+                   ()->assertEquals( 14,numberOfProfSkills ),
+                   ()->assertEquals( "Gladiatorka – Srebro 2",
+                                     hero.getProfession().getProfessionPath(Gender.FEMALE) ));
     }
 
     @Test
     void finishProfession() {
-    }
 
-    @Test
-    void setSkillsLevelOnNewProfessionLevel() {
-    }
+        progress.finishProfession( 2 );
 
-    @Test
-    void increaseRandomSkillWithMinLevel() {
-    }
+        int[] professionStats = hero.getProfession().getProfessionStats();
+        long numberOfSkills = hero.getKnownSkills()
+                .stream()
+                .filter( Skill::isProfession )
+                .filter( skill -> skill.getLevel() >= MIN_LEVEL_ONE )
+                .count();
 
-    @Test
-    void setStatsDueToNewLevel() {
+        assertEquals( 8,numberOfSkills );
+        assertAll( ()->assertTrue( hero.getStats().getStatAt( professionStats[0] )>=MIN_LEVEL_ONE ),
+                   ()->assertTrue( hero.getStats().getStatAt( professionStats[1] )>=MIN_LEVEL_ONE ),
+                   ()->assertTrue( hero.getStats().getStatAt( professionStats[2] )>=MIN_LEVEL_ONE ));
     }
 
     @Test
     void checkProfessionHistory() {
-    }
+        progress.newProfession( ProfessionFactory.getInstance()
+                                        .create("SZARLATAN/SZARLATANKA",1) );
+        progress.newProfession( ProfessionFactory.getInstance().create("SZARLATAN/SZARLATANKA",2) );
 
-    @Test
-    void experienceLevel() {
-    }
+        int level = progress.checkProfessionHistory(
+                ProfessionFactory.getInstance().create( GLADIATOR_GLADIATORKA, 2 ) );
 
-    @Test
-    void randomBonus() {
-    }
-
-    @Test
-    void increaseRandomSkill() {
+        assertEquals( 1,level );
     }
 
     @DisplayName( "set Profession skills - same profession" )
@@ -72,7 +89,7 @@ class HeroProgressTest {
                 .create( GLADIATOR_GLADIATORKA, 2 );
 
         progress.newProfession( levelTwo );
-        progress.setProfessionSkills();
+//        progress.setProfessionSkills();
 
         long count = hero.getKnownSkills()
                 .stream()
@@ -85,11 +102,11 @@ class HeroProgressTest {
     @DisplayName( "set Profession skills - new profession on level 1" )
     @Test
     void setProfessionSkillsForNew() {
-        Profession thiefLevelOne = ProfessionFactory.getInstance().create( "ZŁODZIEJ/ZŁODZIEJKA", 1 );
+        Profession thief = ProfessionFactory.getInstance().create( "ZŁODZIEJ/ZŁODZIEJKA", 1 );
 
 
-        progress.newProfession( thiefLevelOne );
-        progress.setProfessionSkills();
+        progress.newProfession( thief );
+//        progress.setProfessionSkills();
 
         long count = hero.getKnownSkills()
                 .stream()
